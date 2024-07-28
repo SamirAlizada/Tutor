43e02a08bdaef5f6db38c656693a8ef7c692f96d
SamirAlizada <samir.elizade5555@gmail.com>
2024-07-28 23:01:09 +0400

    Increased the max_length of the 'name' field in the models from 20 to 39.

--- teacher/models.py
+++ teacher/models.py
@@ -4,7 +4,7 @@ from dateutil.relativedelta import relativedelta
 import datetime
 
 class Group(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=39)
 
     def __str__(self):
         return self.name
