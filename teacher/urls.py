3033f6435d6678893d5e2a25a0c93b1a1b11c40f
SamirAlizada <samir.elizade5555@gmail.com>
2024-07-31 22:25:37 +0400

    refactor: Update URL patterns for lesson scheduling and detail views
    - Added date parameters to lesson detail update and delete URLs
    - Renamed and adjusted paths for consistency in lesson scheduling and detail management
    - Reorganized URL patterns for clarity and maintainability

--- teacher/urls.py
+++ teacher/urls.py
@@ -2,35 +2,33 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # List
     path('', daily_list, name='daily_list'),
     path('group_list/', group_list, name='group_list'),
-    path('group/add/', add_group, name='add_group'),
-    path('lesson-schedule/add/', add_lesson_schedule, name='add_lesson_schedule'),
     path('group/<int:group_id>/', group_detail, name='group_detail'),
+    path('pay-day/', pay_day, name='pay_day'),
+    path('calendar/', calendar_view, name='calendar_view'),
+    path('day/<int:year>/<int:month>/<int:day>/', day_detail, name='day_detail'),
+
+    # Add
+    path('group/add/', add_group, name='add_group'),
     path('student/add/', add_student, name='add_student'),
+    path('lesson-schedule/add/', add_lesson_schedule, name='add_lesson_schedule'),
+    path('renew-student/<int:student_id>/', renew_student, name='renew_student'),
 
     # Update
     path('update-group/<int:pk>/', update_group, name='update_group'),
     path('lesson-schedule/update/', add_lesson_schedule, name='update_lesson_schedule'),
     path('student/update/<int:pk>/<int:group_id>/', update_student, name='update_student'),
     path('student/update/pay/<int:pk>/', update_student_pay, name='update_student_pay'),
-
+    path('update/<int:schedule_id>/', update_lesson_schedule, name='update_lesson_schedule'),
+    path('update/<int:schedule_id>/<int:year>/<int:month>/<int:day>/', update_lesson_detail, name='update_lesson_detail'),
+    
     # Delete
     path('delete-group/<int:pk>/', delete_group, name='delete_group'),
     path('delete-student/<int:pk>/', delete_student, name='delete_student'),
-
-    path('pay-day/', pay_day, name='pay_day'),
-    path('calendar/', calendar_view, name='calendar_view'),
-
-    path('day/<int:year>/<int:month>/<int:day>/', day_detail, name='day_detail'),
-
-
-    path('renew-student/<int:student_id>/', renew_student, name='renew_student'),
-
-    path('update/<int:schedule_id>/', update_lesson_schedule, name='update_lesson_schedule'),
-    # path('delete/<int:schedule_id>/<int:year>/<int:month>/<int:day>/', delete_lesson_schedule, name='delete_lesson_schedule'),
+    path('delete/<int:schedule_id>/<int:year>/<int:month>/<int:day>/', delete_lesson_detail, name='delete_lesson_detail'),
     path('delete/<int:schedule_id>/', delete_lesson_schedule, name='delete_lesson_schedule'),
-
 
     # Account
     path('login/', user_login, name='login'),
